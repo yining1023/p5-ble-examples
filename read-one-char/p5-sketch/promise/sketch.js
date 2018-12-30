d60bc7098f4bfd19a431f637deba4cb64a419d2d
Yining Shi <ys2189@nyu.edu>
2018-12-29 23:56:05 -0500

    added promise, async await for read example

---
+++ read-one-char/p5-sketch/promise/sketch.js
@@ -0,0 +1,53 @@
+// Copyright (c) 2018 p5ble
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// The serviceUuid must match the serviceUuid of the device you would like to connect
+const serviceUuid = "19b10010-e8f2-537e-4f6c-d104768a1214";
+let myCharacteristic;
+let myValue = 0;
+let myBLE;
+let isConnected = false;
+
+function setup() {
+  // Create a p5ble class
+  myBLE = new p5ble();
+
+  createCanvas(200, 200);
+  background(250);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+
+  // Write value on the canvas
+  text(myValue, 100, 100);
+
+  // Create a 'Connect' button
+  const connectButton = createButton('Connect')
+  connectButton.mousePressed(connectToBle);
+
+  // Create a 'Read' button
+  const readButton = createButton('Read')
+  readButton.mousePressed(read);
+}
+
+function connectToBle() {
+  // Connect to a device by passing the service UUID
+  myBLE.connect(serviceUuid)
+  .then(characteristics => {
+    console.log('characteristics: ', characteristics);
+    myCharacteristic = characteristics[0];
+    isConnected = true;
+  });
+}
+
+function read() {
+  // Read the value of the characteristic
+  myBLE.read(myCharacteristic).then(value => {
+    console.log('value: ', value);
+    myValue = value;
+    background(250);
+    // Write value on the canvas
+    text(myValue, 100, 100);
+  });
+}
